07d0c94643d820fe1260005c2c4ec0a060c4726b
Callasaurus <c.sullivandrage@outlook.com>
2023-02-07 14:29:45 +0000

    initial commit for deployment

---
+++ percayso-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// https://gnews.io/api/v4/search?q=example&apikey=212ecaad787891599f0e5ab9a97298a8&lang=en&country=us&max=10
